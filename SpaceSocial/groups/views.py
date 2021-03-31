3b4ff66eb663fdbc56d4816f6148c5e8591d6bd3
cnulenka <sinulenka001@gmail.com>
2021-04-01 03:01:17 +0530

    added views and html code for groups app

--- SpaceSocial/groups/views.py
+++ SpaceSocial/groups/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
 
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from groups.models import Group,GroupMember
+from . import models
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ("name", "description")
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
